c441b74ce8095614a1041f0df52fa789e9a79131
Jhoan Mauricio Vivas Rubiano <raiman114@gmail.com>
2021-11-16 11:33:04 -0500

    adding basic angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { PersonInputComponent } from './person/person-input.component';
+import { PersonsComponent } from './person/persons.component';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: '', component: PersonsComponent
+  },
+  {
+    path: 'input', component: PersonInputComponent
   },
 ];
 
